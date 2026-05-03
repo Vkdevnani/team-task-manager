1267766c9c31937d2ed1f716820fc2965fe2212c
Vkdevnani <vkdevnani@gmail.com>
2026-05-03 19:30:33 +0530

    Implemented authentication: JWT (signup / login)

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -2,8 +2,11 @@ from app.core.config import settings
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-engine = create_engine(settings.DATABASE_URL)
+db_url = settings.DATABASE_URL.replace("postgresql://", "postgresql+psycopg2://")
+engine = create_engine(db_url)
 
 SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
 
 Base = declarative_base()
+
+print("DB URL:", settings.DATABASE_URL)
--- backend/main.py
+++ backend/main.py
@@ -4,10 +4,16 @@ from app.db.database import Base, engine
 
 from app.models import user, project, task
 
+from app.api import auth
+
 app = FastAPI()
 
+print("Creating tables...")
 Base.metadata.create_all(bind = engine)
+print("done...")
+
+app.include_router(auth.router, prefix = "/auth", tags = ["Auth"])
 
 @app.get("/")
 def root():
-    return {"message": "API is running, database connected and tables created"}
+    return {"message": "API is running..."}
